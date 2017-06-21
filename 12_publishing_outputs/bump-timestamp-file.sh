3096d0a397502ad481da3df49076db565d59eb29
dhaneshande <dande@miraclesoft.com>
2017-06-21 23:13:12 +0530

    Update bump-timestamp-file.sh

--- 12_publishing_outputs/bump-timestamp-file.sh
+++ 12_publishing_outputs/bump-timestamp-file.sh
@@ -9,7 +9,7 @@ cd updated-gist
 echo $(date) > bumpme
 
 git config --global user.email "dande@miraclesoft.com"
-git config --global user.name "dhaneshande"
+git config --global user.name "github123"
 
 git add .
 git commit -m "Bumped date"
