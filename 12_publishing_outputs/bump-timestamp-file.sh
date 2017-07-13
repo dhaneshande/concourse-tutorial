312d2eb7b1d60445f18b3f98c5a0eed6d6aa9338
dhaneshande <dande@miraclesoft.com>
2017-07-13 23:01:17 +0530

    Update bump-timestamp-file.sh

--- 12_publishing_outputs/bump-timestamp-file.sh
+++ 12_publishing_outputs/bump-timestamp-file.sh
@@ -3,13 +3,13 @@
 set -e # fail fast
 set -x # print commands
 
-pwd
-ls -R
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-  
-echo $ PACKAGE_VERSION
+git clone resource-gist updated-gist
+
+cd updated-gist
+echo $(date) > bumpme
+
+git config --global user.email "dande@miraclesoft.com"
+git config --global user.name "dhaneshande"
+
+git add .
+git commit -m "Bumped date"
