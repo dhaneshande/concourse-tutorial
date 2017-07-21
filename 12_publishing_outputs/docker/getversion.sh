efb75b381e5c991d6531a4ba4e7ebee85c024533
dhaneshande <dande@miraclesoft.com>
2017-07-22 00:11:57 +0530

    Add files via upload

---
+++ 12_publishing_outputs/docker/getversion.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e # fail fast
+set -x # print commands
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+  
+echo $ PACKAGE_VERSION
