744bb7bac662a0ff08cd859e5dc727b3bb6a05fa
dhaneshande <dande@miraclesoft.com>
2017-07-24 16:56:17 +0530

    Update bump-timestamp-file.sh

--- 12_publishing_outputs/bump-timestamp-file.sh
+++ 12_publishing_outputs/bump-timestamp-file.sh
@@ -6,7 +6,7 @@ set -x # print commands
 cd resource-tutorial/12_publishing_outputs/
 ls -R
 ls -la
-git clone resource-tutorial updated-gist
+git clone resource-gist updated-gist
 
 cd updated-gist
 ls -R
